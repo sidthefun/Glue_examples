f55e16fd299103721dcf7042167417e47dd00402
sidthefun <yousiddharth@gmail.com>
2023-07-10 21:43:13 +0530

    Glue_examples

---
+++ read_S3_dynamicframe_from_options.py
@@ -0,0 +1,30 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Read CSV file from S3 into a dynamic frame
+dynamic_frame_0 = glueContext.create_dynamic_frame_from_options(
+    connection_type = "s3",
+    connection_options= {"paths": ["s3://sidsagar1/data/simple_csv_data.csv"]},
+    format = "csv",
+    format_options = {
+        "withHeader": True
+    }
+)
+
+# Show / log the records from the dynamic frame. Confirming the read was successful
+dynamic_frame_0.show()
+
+job.commit()
